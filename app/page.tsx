64c869294a7c8b2ec485f084457d2976cedd8ab0
Nischal Tamang <nischaltamang@dhcp-10-250-159-134.harvard.edu>
2026-05-04 16:24:20 -0400

    Chore: fix page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HouseIcon from "../components/houseIcons";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,19 +10,19 @@ export default function Home() {
       </h1>
 
       <div className="grid grid-cols-5  text-black gap-y-12 gap-x-16">
-        <HouseIcon name="Adams" imageSrc="/Adams.png" />
-        <HouseIcon name="Berg" imageSrc="/Berg.png" />
-        <HouseIcon name="Cabot" imageSrc="/Cabot.png" />
-        <HouseIcon name="Currier" imageSrc="/Currier.png" />
-        <HouseIcon name="Dunster" imageSrc="/Dunster.png" />
-        <HouseIcon name="Eliot" imageSrc="/Eliot.png" />
-        <HouseIcon name="Krickland" imageSrc="/Krickland.png" />
-        <HouseIcon name="Leverett" imageSrc="/Lev.png" />
-        <HouseIcon name="Lowell" imageSrc="/Lowell.png" />
-        <HouseIcon name="Mather" imageSrc="/Mather.png" />
-        <HouseIcon name="Pfoho" imageSrc="/Pfoho.png" />
-        <HouseIcon name="Winthrop" imageSrc="/Winthrop.png" />
-        <HouseIcon name="Quincy" imageSrc="/Quincy.png" />
+        <Link href="/Adams">  <HouseIcon name="Adams" imageSrc="/Adams.png" /></Link>
+        <Link href="/Berg">  <HouseIcon name="Berg" imageSrc="/Berg.png" /></Link>
+        <Link href="/Cabot">  <HouseIcon name="Cabot" imageSrc="/Cabot.png" /></Link>
+        <Link href="/Currier">  <HouseIcon name="Currier" imageSrc="/Currier.png" /></Link>
+        <Link href="/Dunster">  <HouseIcon name="Dunster" imageSrc="/Dunster.png" /></Link>
+        <Link href="/Eliot">  <HouseIcon name="Eliot" imageSrc="/Eliot.png" /></Link>
+        <Link href="/Krickland">  <HouseIcon name="Krickland" imageSrc="/Krickland.png" /></Link>
+        <Link href="/Leverett">  <HouseIcon name="Leverett" imageSrc="/Lev.png" /></Link>
+        <Link href="/Lowell">  <HouseIcon name="Lowell" imageSrc="/Lowell.png" /></Link>
+        <Link href="/Mather">  <HouseIcon name="Mather" imageSrc="/Mather.png" /></Link>
+        <Link href="/Pfoho">  <HouseIcon name="Pfoho" imageSrc="/Pfoho.png" /></Link>
+        <Link href="/Winthrop">  <HouseIcon name="Winthrop" imageSrc="/Winthrop.png" /></Link>
+        <Link href="/Quincy">  <HouseIcon name="Quincy" imageSrc="/Quincy.png" /></Link>
       </div>
     </div>
   );
